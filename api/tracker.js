9a7beb1bb1a3254a4e34888e2ddc27ff7c9e874b
Omar Hussein <omarmohammad1951@gmail.com>
2024-11-27 23:17:00 -0500

    tracker update for analytics

--- api/tracker.js
+++ api/tracker.js
@@ -1,35 +1,70 @@
-import { config as env_config } from 'dotenv';
-import { readFileSync } from 'fs';
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
-import express from 'express';
-import { Schema, model } from 'mongoose';
-import cors from 'cors';
-import { connect as mongoose_connect, set as mongoose_set } from 'mongoose';
-import axios from 'axios';
-import hb from 'handlebars';
-import { createTransport } from 'nodemailer';
-import { mw as express_ip } from 'request-ip';
-import ua_parser from 'ua-parser-js';
+import { config as env_config } from "dotenv";
+import { readFileSync } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import express from "express";
+import { Schema, model } from "mongoose";
+import cors from "cors";
+import { connect as mongoose_connect, set as mongoose_set } from "mongoose";
+import axios from "axios";
+import hb from "handlebars";
+import { createTransport } from "nodemailer";
+import { mw as express_ip } from "request-ip";
+import ua_parser from "ua-parser-js";
 
 // START General Setup
 env_config();
 
 // Database setup
 const IPSchema = new Schema({
-  ip: { type: String, unique: true, required: true, immutable: true },
+  ip: { type: String, required: true, immutable: true },
   timestamp: {
     type: Date,
     required: true,
     immutable: true,
-  }
+  },
+  district: String,
+  city: String,
+  regionName: String,
+  country: String,
+  continent: String,
+  zip: String,
+  isp: String,
+  org: String,
+  as: String,
+  mobile: Boolean,
+  proxy: Boolean,
+  hosting: Boolean,
+  coordinates: {
+    lat: Number,
+    lon: Number,
+  },
+  origin: String,
+  ua: String,
+  os: {
+    name: String,
+    version: String,
+  },
+  browser: {
+    name: String,
+    version: String,
+    major: String,
+  },
+  device: {
+    model: String,
+    type: String,
+    vendor: String,
+  },
+  cpu: {
+    architecture: String,
+  },
 });
-const IP_model = model('ip', IPSchema);
+const IP_model = model("ip", IPSchema);
 const database = process.env.MONGO_URI;
-mongoose_set('strictQuery', true);
+mongoose_set("strictQuery", true);
 mongoose_connect(database, {
   useUnifiedTopology: true,
-  useNewUrlParser: true
+  useNewUrlParser: true,
 }).catch((err) => {
   throw new Error(err);
 });
@@ -39,21 +74,21 @@ const sender_email = process.env.SENDER_EMAIL;
 const sender_password = process.env.SENDER_PASSWORD;
 const receiver_email = process.env.RECEIVER_EMAIL;
 const transporter = createTransport({
-  service: 'gmail',
+  service: "gmail",
   auth: {
     user: sender_email,
-    pass: sender_password
-  }
+    pass: sender_password,
+  },
 });
 
 // Handlebars setup
-hb.registerHelper('ifAnd', function (v1, v2, options) {
+hb.registerHelper("ifAnd", function (v1, v2, options) {
   if (v1 && v2) {
     return options.fn(this);
   }
   return options.inverse(this);
 });
-hb.registerHelper('ifOr', function (v1, v2, v3, options) {
+hb.registerHelper("ifOr", function (v1, v2, v3, options) {
   if (v1 || v2 || v3) {
     return options.fn(this);
   }
@@ -62,13 +97,13 @@ hb.registerHelper('ifOr', function (v1, v2, v3, options) {
 
 // Server setup
 const date_options = {
-  weekday: 'short',
-  year: 'numeric',
-  month: 'short',
-  day: 'numeric',
-  hour: 'numeric',
-  minute: 'numeric',
-  timeZone: 'America/Toronto'
+  weekday: "short",
+  year: "numeric",
+  month: "short",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  timeZone: "America/Toronto",
 };
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
@@ -76,7 +111,7 @@ const router = express.Router();
 
 // END General Setup
 
-router.get('*', async (req, res) => {
+router.get("*", async (req, res) => {
   try {
     const { authorization } = req.headers;
     if (authorization !== process.env.AUTHORIZATION) {
@@ -85,43 +120,54 @@ router.get('*', async (req, res) => {
     }
 
     const timestamp = new Date();
-    const ip = req.clientIp.split(':').pop();
+    const ip = req.clientIp.split(":").pop();
     const ip_info = (
       await axios.get(`http://ip-api.com/json/${ip}?fields=18575355`)
     ).data;
-    const origin = req.get('origin') || req.get('host');
+    const origin = `${req.get("origin") || req.get("host")}${req.originalUrl}`;
     const flag = `https://flagcdn.com/24x18/${ip_info?.countryCode?.toLowerCase()}.png`;
     const client_info = {
+      ip,
       ...ip_info,
       origin,
-      flag
+      flag,
     };
-    const user_agent = ua_parser(req.get('user-agent'));
+    const user_agent = ua_parser(req.get("user-agent"));
     const { lat, lon } = client_info;
     const mapUrl =
       lat && lon
         ? `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lon}&zoom=11&size=300x400&maptype=roadmap&markers=color:red%7C${lat},${lon}&key=${process.env.GOOGLE_API_KEY}`
-        : '';
+        : "";
 
-    await IP_model.create({ ip, timestamp });
+    await IP_model.create({ ...client_info, ...user_agent, timestamp });
 
     const html = readFileSync(
-      join(__dirname, '../public', 'email.hbs'),
-      'utf-8'
+      join(__dirname, "../public", "email.hbs"),
+      "utf-8"
     );
     const compiled = hb.compile(html);
+    const existingRecord = await IP_model.findOne({
+      ip: ip,
+      timestamp: { $gte: new Date(Date.now() - 10 * 60 * 1000) },
+    });
+
+    if (existingRecord) {
+      res.sendStatus(200);
+      return;
+    }
+
     const email_content = compiled({
       ...client_info,
       ...user_agent,
-      timestamp: timestamp.toLocaleString('en-US', date_options),
-      mapUrl
+      timestamp: timestamp.toLocaleString("en-US", date_options),
+      mapUrl,
     });
 
     await transporter.sendMail({
       from: sender_email,
       to: receiver_email,
       subject: `New Website Visitor from ${client_info?.city}, ${client_info?.country}!`,
-      html: email_content
+      html: email_content,
     });
     res.sendStatus(200);
   } catch (err) {
@@ -130,7 +176,7 @@ router.get('*', async (req, res) => {
   }
 });
 
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express_ip());
